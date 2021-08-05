e9ae7963fb6d67711835ff598e4f36f38868bcc3
Jennie Sprague <jennifer5599@gmail.com>
2021-08-04 18:23:38 -0700

    session storage in add budget working

--- Phase 1/team_budget_planner/script.js
+++ Phase 1/team_budget_planner/script.js
@@ -3,17 +3,19 @@ var newBudget = {
     projectName : null,
     budget : 03
 }
+var a = 0;
 
 function addBudget() {
     var thisBudget = Object.create(newBudget);
-    thisBudget.clientName = document.getElementById("client");
-    thisBudget.projectName = document.getElementById("project");
-    thisBudget.budget = document.getElementById("budget");
+    thisBudget.clientName = document.getElementById("client").value;
+    thisBudget.projectName = document.getElementById("project").value;
+    thisBudget.budget = document.getElementById("budget").value;
     
-
+    sessionStorage.setItem(a, JSON.stringify(thisBudget));
     //var user = {'name':'John'};
     //sessionStorage.setItem('user', JSON.stringify(user));
     //var obj = JSON.parse(sessionStorage.user);
-
+    console.log(sessionStorage.getItem(a));
+    a++;
     return thisBudget;
  }
