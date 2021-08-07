a065847517b0e1f0c4e9bc6ee7fee38196e7d8af
Jennie Sprague <jsprague@sdsu.edu>
2021-08-06 17:34:28 -0700

    image displaying

--- Phase 1/simply_blogging/startbootstrap-clean-blog-gh-pages/js/blogScript.js
+++ Phase 1/simply_blogging/startbootstrap-clean-blog-gh-pages/js/blogScript.js
@@ -6,17 +6,60 @@ var blog = {
 
 function addBlog() {
   console.log("added blog");
+  var blogStart = document.getElementById('postBlog');
+
+  var title = document.getElementById("title").value;
+  var title_h2 = document.createElement('h2');
+  //var h2 = "<h2>" + title + "</h2>;
+  blogStart.insertAdjacentElement('afterbegin',title_h2);
+
+
+
+  const list = document.createElement("ul");
+  const li = document.createElement("li");
+  list.appendChild(li);
+  var img = document.createElement("img");
+  img.src = URL.createObjectURL(document.getElementById('image').files[0]);
+  img.height = 60;
+  img.onload = function() {
+    URL.revokeObjectURL(this.src);
+  }
+
+  li.appendChild(img);
+  blogStart.appendChild(list);
+
+
+/*
+  var file_selected = document.getElementById('image').files[0];
+  var new_image = document.createElement('img');
+  new_image.classList.add('obj');
+  new_image.file = file_selected;
+  blogStart.appendChild(new_image);
+  const reader = new FileReader();
+  reader.onload = (function(aImg) {
+    return function(e) {
+      aImg.src = e.target.result;
+    };
+  })(new_image);
+  reader.readAsDataURL(file_selected);
+*/
 
   blog.title = document.getElementById("title").value;
+  //var title = document.createElement('h2');
+  var title_text = document.createTextNode(blog.title);
+  //title.appendChild(title_text);
   blog.articles = document.getElementById("articles").value;
-  blog.image = document.getElementById("image").files[0];
+  //blog.image = document.getElementById("image").files[0];
   //const selectedFile = document.getElementById('input').files[0];
+  //var title = 'Title: ' + blog.title;
 
-  var blogStart = document.getElementById('postBlog');
   //blogStart.insertAdjacentHTML('beforeend',"<h2 class='section-heading'>blog.title</h2><p>blog.articles</p>")
   /*" <!--<img class='img-fluid' src='blog.image' /></a>" -->*/
   blogStart.insertAdjacentHTML("beforeend", blog.title);
+  //blogStart.insertAdjacentHTML("beforeend", title);
+  blogStart.insertAdjacentHTML("beforeend", '<br/><br/>')
   blogStart.insertAdjacentText("beforeend", blog.articles);
+  blogStart.insertAdjacentHTML("beforeend", '<br/><br/>')
   /*let cardContainer = null;
   let card = document.createElement('div');
   card.className = 'card shadow cursor-pointer';
@@ -36,10 +79,10 @@ function addBlog() {
   //var s = document.getElementById("mySpan");
   //var h = document.getElementById("myH2");
   //make card
-  var img = document.createElement('img');
-  img.src = blog.image.value;
+  //var img = document.createElement('img');
+  //img.src = blog.image;
   //insert card
-  blogStart.insertAdjacentElement("beforeend", img);
+  //blogStart.insertAdjacentElement("beforeend", img);
   //blogStart.insertAdjacent("beforeend", blog.image.value);
   //blog.image.src = URL.createObjectURL(event.target.files[0]);
   //blog.image.src = url.createObjectURL()
