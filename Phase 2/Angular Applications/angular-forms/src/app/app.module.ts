c17a4efa46b1ce388962731b9810b72e36df58f8
Jennie Sprague <jsprague@sdsu.edu>
2021-08-11 07:10:10 -0700

    edits angular apps

--- Phase 2/Angular Applications/angular-forms/src/app/app.module.ts
+++ Phase 2/Angular Applications/angular-forms/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChildComponent } from './child.component';
+
 @NgModule({
   declarations: [
-    AppComponent, ChildComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Phase 2/demo-app/src/app/child.component.ts
+++ Phase 2/demo-app/src/app/child.component.ts
@@ -2,7 +2,9 @@ import { Component } from "@angular/core";
 
 
 @Component({
-    selector:"app-child"
+    selector:"app-child",
+    templateUrl:"./child.component.html",
+    styleUrls:["./child.component.css"]
 })
 export class ChildComponent {
     msg:string="This is child component";
