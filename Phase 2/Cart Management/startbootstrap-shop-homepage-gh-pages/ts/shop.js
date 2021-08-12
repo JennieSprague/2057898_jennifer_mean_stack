0523b81a7d654f358da634c0f98f2f1ff3c8c688
Jennie Sprague <jsprague@sdsu.edu>
2021-08-12 16:34:23 -0700

    checkout -- passing and parsing array correctly

--- Phase 2/Cart Management/startbootstrap-shop-homepage-gh-pages/ts/shop.js
+++ Phase 2/Cart Management/startbootstrap-shop-homepage-gh-pages/ts/shop.js
@@ -20,6 +20,7 @@ var product5 = new Product('Birthday Cat', 10.00);
 var product6 = new Product('Baby Fox', 10.00);
 var product7 = new Product('Humpty Dumpty', 10.00);
 var product8 = new Product('Baby Peacock', 10.00);
+//array of 8 products
 var products = [];
 products.push(product1);
 products.push(product2);
@@ -29,13 +30,57 @@ products.push(product5);
 products.push(product6);
 products.push(product7);
 products.push(product8);
+//cart array for user to add products to
 var cart = [];
+var table = document.getElementById("cart_table");
 function add(index) {
     console.log('added to cart');
     console.log(index);
     console.log(products[index]);
+    //update number of items in cart
     cart_size++;
     document.getElementById("cart_size").innerHTML = cart_size.toString();
-    //session storage products[index]
+    //add product to cart array
     cart.push(products[index]);
+    sessionStorage.setItem(products[index].name, JSON.stringify(products[index]));
+    localStorage.setItem("cart", JSON.stringify(cart));
+    //sessionStorage.setItem(cart_size.toString , JSON.stringify(products[index]));
+}
+function checkout() {
+    var checkout_cart = JSON.parse(localStorage.getItem("cart"));
+    console.log(checkout_cart);
+    console.log(checkout_cart[0]);
+    console.log(checkout_cart.length);
+    for (var i = 0; i < checkout_cart.length; i++) {
+        var curr_product_name = checkout_cart[i].name;
+        console.log("product name");
+        console.log(curr_product_name);
+        var curr_product_price = checkout_cart[i].price;
+        console.log("product price");
+        console.log(curr_product_price);
+    }
+    //console.log(Object.keys(sessionStorage));
+    //let item = JSON.parse(sessionStorage.getItem)
+    //let product_name = cart
+    // cart.forEach(function(items){
+    //     var row = document.createElement("tr");
+    //     items.forEach(function(item) {
+    //       var cell = document.createElement("td");
+    //       cell.textContent = item;
+    //       row.appendChild(cell);
+    //     });
+    //     table.appendChild(row);
+    // });
+    // for (let i = 0; i < sessionStorage.length; i++){
+    //     var myArr = JSON.parse(sessionStorage.getItem(i));
+    //     var client = myArr.clientName;
+    //     var project = myArr.projectName;
+    //     var budget = myArr.budget;
+    //     //var num_rows = table_refer.rows.length;
+    //     var curr_row = table_refer.insertRow(-1);
+    //     curr_row.insertCell(0).innerHTML = client;
+    //     curr_row.insertCell(1).innerHTML = project;
+    //     curr_row.insertCell(2).innerHTML = budget;
+    //     total_budget += parseInt(budget);
+    // }
 }
--- Phase 2/Cart Management/startbootstrap-shop-homepage-gh-pages/ts/shop.ts
+++ Phase 2/Cart Management/startbootstrap-shop-homepage-gh-pages/ts/shop.ts
@@ -18,7 +18,7 @@ let product5 = new Product('Birthday Cat', 10.00);
 let product6 = new Product('Baby Fox', 10.00);
 let product7 = new Product('Humpty Dumpty', 10.00);
 let product8 = new Product('Baby Peacock', 10.00);
-
+//array of 8 products
 const products: Product[]=[];
 products.push(product1);
 products.push(product2);
@@ -28,14 +28,62 @@ products.push(product5);
 products.push(product6);
 products.push(product7);
 products.push(product8);
+//cart array for user to add products to
 const cart: Product[]=[];
+let table = document.getElementById("cart_table");
 
 function add(index: number){
     console.log('added to cart');
     console.log(index);
     console.log(products[index]);
+    //update number of items in cart
     cart_size++;
     document.getElementById("cart_size").innerHTML = cart_size.toString();
-    //session storage products[index]
+    //add product to cart array
     cart.push(products[index]);
+    sessionStorage.setItem(products[index].name, JSON.stringify(products[index]));
+    localStorage.setItem("cart", JSON.stringify(cart));
+    //sessionStorage.setItem(cart_size.toString , JSON.stringify(products[index]));
+}
+
+function checkout(){
+    let checkout_cart = JSON.parse(localStorage.getItem("cart"));
+    console.log(checkout_cart);
+    //console.log(checkout_cart[0]);
+    //console.log(checkout_cart.length);
+    for (let i = 0; i < checkout_cart.length; i++){
+        let curr_product_name = checkout_cart[i].name;
+        console.log("product name");
+        console.log(curr_product_name);
+        let curr_product_price = checkout_cart[i].price;
+        console.log("product price");
+        console.log(curr_product_price);
+    }
+
+    //console.log(Object.keys(sessionStorage));
+    //let item = JSON.parse(sessionStorage.getItem)
+    //let product_name = cart
+    // cart.forEach(function(items){
+    //     var row = document.createElement("tr");
+    //     items.forEach(function(item) {
+    //       var cell = document.createElement("td");
+    //       cell.textContent = item;
+    //       row.appendChild(cell);
+    //     });
+    //     table.appendChild(row);
+    // });
+
+    // for (let i = 0; i < sessionStorage.length; i++){
+    //     var myArr = JSON.parse(sessionStorage.getItem(i));
+    //     var client = myArr.clientName;
+    //     var project = myArr.projectName;
+    //     var budget = myArr.budget;
+    //     //var num_rows = table_refer.rows.length;
+    //     var curr_row = table_refer.insertRow(-1);
+    //     curr_row.insertCell(0).innerHTML = client;
+    //     curr_row.insertCell(1).innerHTML = project;
+    //     curr_row.insertCell(2).innerHTML = budget;
+    //     total_budget += parseInt(budget);
+    // }
+
 }
