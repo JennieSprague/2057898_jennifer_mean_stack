234e3f16aa789fa790af1f2105d8e87cefbafb68
Jennie Sprague <jsprague@sdsu.edu>
2021-08-12 17:03:24 -0700

    clean , rm superfluous comments

--- Phase 2/Cart Management/startbootstrap-shop-homepage-gh-pages/ts/shop.ts
+++ Phase 2/Cart Management/startbootstrap-shop-homepage-gh-pages/ts/shop.ts
@@ -7,9 +7,6 @@ class Product {
     }
 }
 
-//products[]
-//product.push(Product)
-//(onClick) cart.push(Product)
 let product1 = new Product('Halloween Bats', 10.00);
 let product2 = new Product('Bee Heart', 10.00);
 let product3 = new Product('Camping Bears', 10.00);
@@ -43,7 +40,6 @@ function add(index: number){
     cart.push(products[index]);
     sessionStorage.setItem(products[index].name, JSON.stringify(products[index]));
     localStorage.setItem("cart", JSON.stringify(cart));
-    //sessionStorage.setItem(cart_size.toString , JSON.stringify(products[index]));
 }
 
 function checkout(){
@@ -53,50 +49,19 @@ function checkout(){
     let my_table = document.getElementById("cart_table");
     let tbody = document.createElement("tbody");
     my_table.appendChild(tbody);
-    //console.log(checkout_cart[0]);
-    //console.log(checkout_cart.length);
     for (let i = 0; i < checkout_cart.length; i++){
         let row = document.createElement("tr");
         let cell = document.createElement("td");
         let curr_product_name = checkout_cart[i].name;
         cell.textContent =curr_product_name;
         row.appendChild(cell);
-        //console.log("product name");
-        //console.log(curr_product_name);
         let curr_product_price = checkout_cart[i].price;
         total_price += curr_product_price;
         let cell2 = document.createElement("td");
         cell2.textContent =curr_product_price;
         row.appendChild(cell2);
-        //console.log("product price");
-        //console.log(curr_product_price);
         tbody.appendChild(row);
     }
     document.getElementById("total").innerHTML = "Total : " + total_price.toString();
-    //console.log(Object.keys(sessionStorage));
-    //let item = JSON.parse(sessionStorage.getItem)
-    //let product_name = cart
-    // cart.forEach(function(items){
-    //     var row = document.createElement("tr");
-    //     items.forEach(function(item) {
-    //       var cell = document.createElement("td");
-    //       cell.textContent = item;
-    //       row.appendChild(cell);
-    //     });
-    //     table.appendChild(row);
-    // });
-
-    // for (let i = 0; i < sessionStorage.length; i++){
-    //     var myArr = JSON.parse(sessionStorage.getItem(i));
-    //     var client = myArr.clientName;
-    //     var project = myArr.projectName;
-    //     var budget = myArr.budget;
-    //     //var num_rows = table_refer.rows.length;
-    //     var curr_row = table_refer.insertRow(-1);
-    //     curr_row.insertCell(0).innerHTML = client;
-    //     curr_row.insertCell(1).innerHTML = project;
-    //     curr_row.insertCell(2).innerHTML = budget;
-    //     total_budget += parseInt(budget);
-    // }
 
 }
