1ce62dcc7f25a6fcb945646f4ccf0dc68fa966b7
Jennie Sprague <jsprague@sdsu.edu>
2021-08-16 22:20:47 -0700

    added tdf-login-page

---
+++ Phase 2/Portfolio/portfolio-app/src/app/tdf-login-page/tdf-login-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tdf-login-page',
+  templateUrl: './tdf-login-page.component.html',
+  styleUrls: ['./tdf-login-page.component.css']
+})
+export class TdfLoginPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
