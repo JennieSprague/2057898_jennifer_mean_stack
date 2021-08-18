57bd1d8e6a53b55c043269db9555d6a34b2c3079
Jennie Sprague <jsprague@sdsu.edu>
2021-08-18 14:51:13 -0700

    Sign in mdf setup

--- Phase 2/Task Tracker/task-track-app/src/app/app-routing.module.ts
+++ Phase 2/Task Tracker/task-track-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"signin",component:SigninComponent},
+  {path:"signup",component:SignupComponent},
+  {path:"task-list/:uname",component:TaskListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Phase 2/Task Tracker/task-track-app/src/app/app.module.ts
+++ Phase 2/Task Tracker/task-track-app/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
