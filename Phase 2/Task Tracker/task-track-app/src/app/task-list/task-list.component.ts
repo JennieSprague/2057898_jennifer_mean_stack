b52eaf7b85bbcc9e630ee2b362887776e75dc554
Jennie Sprague <jsprague@sdsu.edu>
2021-08-18 17:18:13 -0700

    project made

--- Phase 2/Task Tracker/task-track-app/src/app/task-list/task-list.component.ts
+++ Phase 2/Task Tracker/task-track-app/src/app/task-list/task-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { isJSDocNameReference } from 'typescript';
+import { Task } from '../task';
 
 @Component({
   selector: 'app-task-list',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
+  taskRef = new FormGroup({
+    
+  });
+  tasks:Array<Task>=new Array();
+  user?:string=""
 
-  constructor() { }
-
+  constructor(public activeRoute:ActivatedRoute) { 
+    this.activeRoute.params.subscribe(data=>this.user=data.uname);
+  } 
   ngOnInit(): void {
   }
 
+  addTask(id:string, username:string, task:string, deadline:string){
+    let newTask = new Task(id, username, task, deadline);
+      this.tasks.push(newTask);
+  }
+  
+  reset(){
+    
+  }
+  // addTask(){
+  //   let newTask = this.taskRef;
+  // }
 }
